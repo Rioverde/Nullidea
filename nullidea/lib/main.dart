da26409a70d5b9ab02f01776344820d296fd3442
rioverde <abbasbabayev.m98@gmail.com>
2020-03-07 12:57:06 +0200

    Added forget password, connected to firebase added buttton

--- nullidea/lib/main.dart
+++ nullidea/lib/main.dart
@@ -25,64 +25,89 @@ class Nullidea extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: buildTheme(),
-        home: Scaffold(
-          backgroundColor: Colors.black,
-          body: SafeArea(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    'Nullidea',
+      theme: buildTheme(),
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Nullidea',
+                  style: TextStyle(
+                      fontFamily: 'Pacifico',
+                      fontSize: 60.0,
+                      color: PrimaryColor),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+                  child: TextFormField(
+                    textAlign: TextAlign.start,
+                    style: TextStyle(color: Colors.white),
+                    cursorColor: PrimaryColor,
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.person, color: PrimaryColor),
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(color: PrimaryColor),
+                          borderRadius: BorderRadius.all(Radius.circular(8))),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: PrimaryColor),
+                          borderRadius: BorderRadius.all(Radius.circular(8))),
+                      // labelText: 'Email',
+                      // labelStyle: TextStyle(color: PrimaryColor),
+                      hintText: "Email",
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextFormField(
+                    obscureText: true,
+                    textAlign: TextAlign.start,
+                    style: TextStyle(color: Colors.white),
+                    cursorColor: PrimaryColor,
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.lock, color: PrimaryColor),
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(color: PrimaryColor),
+                          borderRadius: BorderRadius.all(Radius.circular(8))),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: PrimaryColor),
+                          borderRadius: BorderRadius.all(Radius.circular(8))),
+                      // labelText: 'Password',
+                      // labelStyle: TextStyle(color: PrimaryColor),
+                      hintText: "Password",
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(0, 0, 16, 0),
+                  alignment: Alignment.topRight,
+                  child: Text(
+                    "Forgot password ?",
                     style: TextStyle(
-                        fontFamily: 'Pacifico',
-                        fontSize: 60.0,
-                        color: PrimaryColor),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: TextFormField(
-                      textAlign: TextAlign.start,
-                      style: TextStyle(color: Colors.white),
-                      cursorColor: PrimaryColor,
-                      decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.person, color: PrimaryColor),
-                        border: OutlineInputBorder(
-                            borderSide: BorderSide(color: PrimaryColor),
-                            borderRadius: BorderRadius.all(Radius.circular(8))),
-                        enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: PrimaryColor),
-                            borderRadius: BorderRadius.all(Radius.circular(8))),
-                        labelText: 'Email',
-                        labelStyle: TextStyle(color: PrimaryColor),
-                        hintText: "Email adress",
-                      ),
+                      color: PrimaryColor,
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: TextFormField(
-                      obscureText: true,
-                      textAlign: TextAlign.start,
-                      style: TextStyle(color: Colors.white),
-                      cursorColor: PrimaryColor,
-                      decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.lock, color: PrimaryColor),
-                        border: OutlineInputBorder(
-                            borderSide: BorderSide(color: PrimaryColor),
-                            borderRadius: BorderRadius.all(Radius.circular(8))),
-                        enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: PrimaryColor),
-                            borderRadius: BorderRadius.all(Radius.circular(8))),
-                        labelText: 'Password',
-                        
-                        labelStyle: TextStyle(color: PrimaryColor),
-                        hintText: "Password",
-                      ),
-                    ),
-                  )
-                ]),
-          ),
-        ));
+                ),
+                FlatButton(
+                  color: PrimaryColor,
+                  textColor: Colors.black,
+                  disabledColor: Colors.grey, 
+                  disabledTextColor: Colors.black,
+                  padding: EdgeInsets.all(16.0),
+                  splashColor: Colors.blueAccent,
+                  onPressed: () {
+                    /*...*/
+                  },
+                  child: Text(
+                    "Flat Button",
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                )
+              ]),
+        ),
+      ),
+    );
   }
 }
