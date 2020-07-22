ee79091fdc9f5acfaf0cd58c70ba6af53951858f
rioverde <abbasbabayev.m98@gmail.com>
2020-07-22 23:07:03 +0300

    splash screen added

--- nullidea/lib/screens/splash.dart
+++ nullidea/lib/screens/splash.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:nullidea/screens/Login/login.dart';
 import 'package:nullidea/theme.dart';
 import 'package:splashscreen/splashscreen.dart';
@@ -12,7 +13,8 @@ class _SplashState extends State<Splash> {
   @override
   Widget build(BuildContext context) {
     return SplashScreen(
-      seconds: 2,
+      seconds: 4,
+      title: nullideaText(),
       navigateAfterSeconds: new Login(),
       backgroundColor: Colors.black,
       styleTextUnderTheLoader: new TextStyle(),
@@ -20,4 +22,12 @@ class _SplashState extends State<Splash> {
       loaderColor: primaryColor,
     );
   }
+}
+
+Text nullideaText() {
+  return Text(
+    "Nullidea",
+    style: GoogleFonts.pacifico(
+        fontSize: 70.0, fontWeight: FontWeight.normal, color: primaryColor),
+  );
 }
